ef2c89f89a0cd3d1bdcfed4cca2db54e824cfbdb
Yanqiao Chen <chen02@allegheny.edu>
2022-05-26 15:57:57 -0400

    Add page parse
    Add new feature. Given url, the html  of the page will be returned

--- news-analysis/article_capture.py
+++ news-analysis/article_capture.py
@@ -1,1 +1,15 @@
 """Capture relative news text from websites"""
+
+import requests
+from bs4 import BeautifulSoup
+
+class pageParse:
+    """Take the HTML from a website."""
+    def __init__(self,url) -> None:
+        self.url = url
+
+    def getText(self):
+        page = requests.get(self.url)
+        return page.content
+
+
