79093bf190b2e37cbc113ba1e1f2e86c14705628
Yanqiao Chen <chen02@allegheny.edu>
2022-06-01 17:41:46 -0400

    Add caller class

--- news_analysis/article_capture.py
+++ news_analysis/article_capture.py
@@ -7,8 +7,8 @@ from selenium.webdriver import Chrome, ChromeOptions
 
 class ArticleSearch:
     """Search articles in the news media according to the query"""
-    def __init__(self, search_word:str) -> None:
-        self.search_word = search_word
+    def __init__(self, search_words:str) -> None:
+        self.search_word = search_words
         # Make browser doesn't show up
         self.options = ChromeOptions()
         self.options.headless = True
@@ -22,16 +22,18 @@ class ArticleSearch:
         elements = self.driver.find_elements_by_class_name("gs-title")
         self.driver.quit
         links = map(lambda x:x.get_attribute("href"),elements)
-        #Avoid repeat contents
+        #Avoid repeat_links
         filtered_links = set(ele for ele in links if ele is not None)
         return filtered_links
 
+
     def CNN_search(self):
-        #Convert into the forms of query as a part of url
+        #Convert into thE forms of query as a part of url
         self.search_word = self.search_word.replace(" ", "%20")
         self.driver.get(f"https://www.cnn.com/search?q={self.search_word}&size=10&sort=relevance")
         elements = self.driver.find_elements_by_class_name("cnn-search__result-headline")
         self.driver.quit
+        #Target the children
         elements = map(lambda x:x.find_element_by_css_selector("*"),elements)
         links = map(lambda x:x.get_attribute("href"),elements)
         #Avoid repeat contents
@@ -69,6 +71,34 @@ class PageParse:
         self.article_texts = " ".join(self.article_texts_list)
         return self.article_texts
 
+class ArticleCaptureController:
+    """Do the article capture with the search words and output the articles"""
+    def __init__(self, search_words) -> None:
+        self.articles_search = ArticleSearch(search_words)
+        self.corpus = []
+
+    def NBC_caller(self):
+        article_links = self.articles_search.NBC_search()
+        for link in article_links:
+            article = PageParse(link)
+            article.getText()
+            article.NBC_filter()
+            polished_article = article.restructure()
+            self.corpus.append(polished_article)
+    def CNN_caller(self):
+        article_links = self.articles_search.CNN_search()
+        for link in article_links:
+            article = PageParse(link)
+            article.getText()
+            article.CNN_filter()
+            polished_article = article.restructure()
+            self.corpus.append(polished_article)
+    
+    def get_corpus(self):
+        self.NBC_caller()
+        self.CNN_caller()
+        return self.corpus
+
 if __name__ == "__main__":
-    nbc = ArticleSearch("china covid")
-    print(nbc.CNN_search())
+    a = ArticleCaptureController("China covid")
+    print(len(a.get_corpus()))
