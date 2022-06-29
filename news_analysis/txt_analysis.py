fd3781ff8293301d30a9877c5a2b6b02c5232214
Yanqiao Chen <chen02@allegheny.edu>
2022-06-29 16:57:57 -0400

    Refactor to add preprocess class

--- news_analysis/txt_analysis.py
+++ news_analysis/txt_analysis.py
@@ -1,81 +1,160 @@
 """Analyze sentiment and term frequency from texts"""
-from operator import index
-from turtle import update
-from matplotlib.pyplot import text
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import re
 import article_capture
 import string
+
 import nltk
+nltk.download("wordnet")
+nltk.download('omw-1.4')
+from nltk.stem import SnowballStemmer, WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+from gensim.corpora import Dictionary
+from gensim.models import LdaModel
+
+import pyLDAvis 
+import pyLDAvis.gensim_models
 
 class CorpusPreprocess:
-    def __init__(self, corpus) -> None:
-        self.corpus = corpus
-    
-    def stop_words_remove(self):
-        pass
-class TfidfAnalysis:
+    """Remove punctuations and words which express no meanings like 'is', 'a',
+        and tokenize texts into forms into words
+    """
     def __init__(self,corpus) -> None:
         self.corpus = corpus
-        self.cleaned_corpus = []
 
-    def stop_words_remove(self):
+    def punctuations_and_stop_words_remove(self):
         with open("news_analysis/data/stop_words.txt") as f:
             stop_words = f.read()
             # Get a list of stop words
             stop_words = stop_words.split(", ")
+
         self.corpus = list(map(lambda x:re.sub(r'[0-9]+', '', x),self.corpus))
-        for text in self.corpus:
+        for idx, text in enumerate(self.corpus):
             # Get rid of all the punctuation
             text = text.translate(str.maketrans("","",string.punctuation))
             words = text.split()
             words = [word for word in words if word.lower() not in stop_words]
-            self.cleaned_corpus.append(" ".join(words))
+            self.corpus[idx]=" ".join(words)
+        return self.corpus
+
 
     def stem_words(self):
-        ps = nltk.SnowballStemmer(language="english")
-        for corpus_idx, text in enumerate(self.cleaned_corpus):
+        ps = SnowballStemmer(language="english")
+        for corpus_idx, text in enumerate(self.corpus):
             words = text.split()
             words = list(map(lambda word: ps.stem(word), words))
-            self.cleaned_corpus[corpus_idx] = " ".join(words)
-            
+            self.corpus[corpus_idx] = " ".join(words)
+        return self.corpus
+
+    # def lemmatize_words(self):
+    #     lemmatizer = WordNetLemmatizer()
+    #     a = ["worked hards ideas to slowly returning","This was a bad ideas"]
+    #     for corpus_idx, text in enumerate(a):
+    #         words = text.split()
+    #         words = list(map(lambda word: lemmatizer.lemmatize(word), words))
+    #         a[corpus_idx] = " ".join(words)
+    #     print(a)
+
+    def process_corpus(self):
+        self.punctuations_and_stop_words_remove()
+        self.stem_words()
+        return self.corpus
+
+class TopicModel:
+    def __init__(self, input_corpus) -> None:
+        self.corpus = input_corpus
+
+    def tokenize_words(self):
+        pass
+        # Split the documents into tokens
+        # tokenizer = RegexpTokenizer(r'\w+')
+        # for idx in range(len(self.corpus)):
+        #     self.corpus[idx] = self.corpus[idx].lower()  # Convert to lowercase
+        #     self.corpus[idx] = tokenizer.tokenize(self.corpus[idx])  # Split into words
+
+        # # Remove numbers, but not words that contain numbers.
+        # self.corpus = [[token for token in doc if not token.isnumeric()] for doc in self.corpus]
+
+        # # Remove words that are only one character.
+        # self.corpus = [[token for token in doc if len(token) > 1] for doc in self.corpus]
+
+    def bag_of_words(self):
+        self.corpus = [words.split() for words in self.corpus ]
+        print(self.corpus)
+        # Create a dictionary representation of the documents.
+        dictionary = Dictionary(self.corpus)
+        # Bag-of-words representation of the documents.
+        corpus = [dictionary.doc2bow(doc) for doc in self.corpus]
+        lda_model = LdaModel(
+            corpus=corpus,
+            id2word=dictionary,
+            chunksize=2000,
+            alpha='auto',
+            eta='auto',
+            iterations=200,
+            num_topics=2,
+            passes=5,
+            eval_every=None
+        )
+        pyLDAvis.enable_notebook()
+        vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary.id2token, mds = "mmds", R = 10)
+        vis
+
+class TfidfAnalysis:
+    def __init__(self,cleaned_corpus) -> None:
+        self.cleaned_corpus = cleaned_corpus
+
 
     def analyze(self):
         vectorizer = TfidfVectorizer(
             lowercase= True,
-            max_features= 20,
+            max_features= 50,
             max_df= 0.8,
-            min_df = 5,
-            ngram_range= (1,2),
-            stop_words="english"
+            min_df = 3,
+            ngram_range= (1,1),
+            stop_words="english",
+            binary= True
         ) 
         vectors = vectorizer.fit_transform(self.cleaned_corpus)
+        # print(vectors)
         token = vectorizer.get_feature_names_out()
         return token
 
     def get_keywords(self):
-        self.stop_words_remove()
         # self.stem_words()
         return self.analyze()
 
 
 class SentimentAnalyzer:
-    """Because of the attribute of Vader analyzer that the lexicon it uses accepts the raw words but not the base word
+    """Because of the attribute of Vader analyzer that the lexicon it uses deals with the raw words but not the base word
     The preprocessed corpus won't be used here"""
-    nltk.download(["vader_lexicon"])
     def __init__(self,corpus) -> None:
+        nltk.download(["vader_lexicon"])
         self.corpus = corpus
     def get_sentiment(self):
         analyzer = SentimentIntensityAnalyzer()
         return analyzer.polarity_scores(self.corpus)
 
+
+class SummaryController:
+    def __init__(self, corpus) -> None:
+        self.corpus = corpus
+
+    def get_analysis(self):
+        Tfidf_summary = TfidfAnalysis(self.corpus)
+        features = Tfidf_summary.get_keywords()
+        sentiment_summary = SentimentAnalyzer(self.corpus[0])
+        return features, sentiment_summary.get_sentiment()
+        
+
 if __name__ == "__main__":
-    a = article_capture.ArticleCaptureController("china covid")
-    print("articles are ready")
-    Tfidf_sample = TfidfAnalysis(a.get_corpus())
+    a = article_capture.ArticleCaptureController("Ukraine","Reuters")
+    corpus = a.get_corpus()           
+    b = CorpusPreprocess(corpus).punctuations_and_stop_words_remove()
+    Tfidf_sample = TfidfAnalysis(b)
     print(Tfidf_sample.get_keywords())
     # sentiment_sample = SentimentAnalyzer(a.get_corpus()[0])
     # print(sentiment_sample.get_sentiment())
