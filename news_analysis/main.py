8f7d3732aad3a11713f683b00a1ffa76e885d1f2
Yanqiao Chen <chen02@allegheny.edu>
2022-06-29 16:58:41 -0400

    Add main function

--- news_analysis/main.py
+++ news_analysis/main.py
@@ -1,1 +1,12 @@
 """Implement the news analysis tool"""
+
+from article_capture import ArticleCaptureController
+from txt_analysis import SummaryController, CorpusPreprocess
+from summary_create import words_graph
+
+if __name__ == "__main__":
+    corpus = ArticleCaptureController("covid china", "CNN").get_corpus()
+    cleaned_corpus = CorpusPreprocess(corpus).punctuations_and_stop_words_remove()
+    summary = SummaryController(corpus).get_analysis()
+    print(summary)
+    words_graph(summary[0])
